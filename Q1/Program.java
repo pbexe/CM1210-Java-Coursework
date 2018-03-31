d1a88ac70beb28169b12dfb07f829ea5311db0f5
Miles Budden <miles@budden.net>
2018-03-31 19:25:44 +0100

    Errors are kinda caught

--- Q1/Program.java
+++ Q1/Program.java
@@ -4,40 +4,54 @@
 
 import java.util.ArrayList;
 import java.util.List;
+
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.PrintWriter;
+import java.lang.ClassNotFoundException;
 
 public class Program {
 
-    public static void main(String[] args) throws IOException{
-        System.out.println("Hello");
+    public static void main(String[] args) throws IOException, ClassNotFoundException{
         List<Student> students = new ArrayList<Student>();
         Student s = new Student("Bob bobby", "C123456", "CompSci", "CM1234", "11a", "Bobby st", "A town", "CF1CF");
         students.add(s);
         Student t = new Student("Miles Budden", "C234567", "CompSci", "CM1202", "11b", "Birchwood Road", "Cardiff", "CF2CF");
         students.add(t);
-        save(true, "file.tsv", students);
+        save(true, "file.dat", students);
+        List<Student> x = load(true, "file.dat");
+        for (Student student : x) {
+            System.out.println(student.getName());
+        }
     }
 
     public static void save(boolean binary, String path, List<Student> students) throws IOException {
-        if (binary){
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("students.dat"));
-            out.writeObject(students);
-            out.close();
-            // throw new UnsupportedOperationException("Haven't done this yet");
-        }
-        else {
-            PrintWriter writer = new PrintWriter(path, "UTF-8");
-            for (Student student : students) {
-                writer.println(student.getName() + "\t" + student.getNumber() + "\t" + student.getCourse() + "\t" + student.getId() + "\t" + student.getHouseNumber() + "\t" + student.getStreetName() + "\t" + student.getTown() + "\t" + student.getPostcode());
+        try{
+            if (binary){
+                ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(path));
+                out.writeObject(students);
+                out.close();
             }
-            writer.close();
+            else {
+                PrintWriter writer = new PrintWriter(path, "UTF-8");
+                for (Student student : students) {
+                    writer.println(student.getName() + "\t" + student.getNumber() + "\t" + student.getCourse() + "\t" + student.getId() + "\t" + student.getHouseNumber() + "\t" + student.getStreetName() + "\t" + student.getTown() + "\t" + student.getPostcode());
+                }
+                writer.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    // public static List load(boolean binary, String path) {
-    //     ;
-    // }
+    public static List<Student> load(boolean binary, String path) throws IOException, ClassNotFoundException{
+        // TODO: Add error checking
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(path));
+        List<Student> s = (List<Student>) in.readObject();
+        in.close();
+        return s;
+    }
 }
