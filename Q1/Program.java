121dea6055623bcdac1c841a5c6bee76ae48f38f
Miles Budden <miles@budden.net>
2018-04-01 16:17:14 +0100

    Started implementing the options

--- Q1/Program.java
+++ Q1/Program.java
@@ -1,10 +1,11 @@
 /**
- * Program
- */
+* Program
+*/
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
-
+import java.util.Scanner;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -17,41 +18,99 @@ public class Program {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException{
         List<Student> students = new ArrayList<Student>();
-        Student s = new Student("Bob bobby", "C123456", "CompSci", "CM1234", "11a", "Bobby st", "A town", "CF1CF");
-        students.add(s);
-        Student t = new Student("Miles Budden", "C234567", "CompSci", "CM1202", "11b", "Birchwood Road", "Cardiff", "CF2CF");
-        students.add(t);
-        save(true, "file.dat", students);
-        List<Student> x = load(true, "file.dat");
-        for (Student student : x) {
-            System.out.println(student.getName());
-        }
-    }
-
-    public static void save(boolean binary, String path, List<Student> students) throws IOException {
-        try{
-            if (binary){
-                ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(path));
-                out.writeObject(students);
-                out.close();
-            }
-            else {
-                PrintWriter writer = new PrintWriter(path, "UTF-8");
-                for (Student student : students) {
-                    writer.println(student.getName() + "\t" + student.getNumber() + "\t" + student.getCourse() + "\t" + student.getId() + "\t" + student.getHouseNumber() + "\t" + student.getStreetName() + "\t" + student.getTown() + "\t" + student.getPostcode());
+            Scanner in = new Scanner(System.in);
+            int choice;
+            while (true){
+                System.out.println("Please select an option:");
+                System.out.println("\t1. Load student data from file");
+                System.out.println("\t2. Save student data to file");
+                System.out.println("\t3. Add a student");
+                System.out.println("\t4. Display all students");
+                System.out.println("\t5. Search students by course");
+                System.out.println("\t6. Search student addresses");
+                System.out.println("\t7. Display subset of students");
+                System.out.println("\t8. Quit");
+                if (in.hasNextInt()){
+                    choice = in.nextInt();
+                    if (choice >= 9 || choice <= 0){
+                        System.out.println("Please enter a valid input");
+                    } else {
+                        switch (choice) {
+                            case 1:
+                                students = optionLoad();
+                                break;
+                            case 2:
+                                optionSave(students);
+                                break;
+                            default:
+                            break;
+                        }
+                    }
+                } else {
+                    System.out.println("Please enter a valid input");
+                    in.next();
                 }
-                writer.close();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        }
+
+        public static void save(boolean binary, String path, List<Student> students) throws IOException {
+            try{
+                if (binary){
+                    ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(path));
+                    out.writeObject(students);
+                    out.close();
+                }
+                else {
+                    PrintWriter writer = new PrintWriter(path, "UTF-8");
+                    for (Student student : students) {
+                        writer.println(student.getName() + "\t" + student.getNumber() + "\t" + student.getCourse() + "\t" + student.getId() + "\t" + student.getHouseNumber() + "\t" + student.getStreetName() + "\t" + student.getTown() + "\t" + student.getPostcode());
+                    }
+                    writer.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static List<Student> load(boolean binary, String path) throws IOException, ClassNotFoundException{
+            // TODO: Add error checking
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream(path));
+            List<Student> s = (List<Student>) in.readObject();
+            in.close();
+            return s;
+        }
+
+        public static List<Student> optionLoad() throws IOException{
+            // Repeatedly try until a valid file is loaded
+            // TODO: Some sort of way to escape `while` loop instead of loading a file
+            while (true){
+                System.out.println("Please enter the path/filename that you wish to load:");
+                Scanner in = new Scanner(System.in);
+                String path = in.nextLine();
+                System.out.println("Is this:\n\t1. A binary file\n\t2. A text file?");
+                int choice;
+                while (true) {
+                    if (in.hasNextInt()) {
+                        choice = in.nextInt();
+                        if (choice >= 3 || choice <= 0) {
+                            System.out.println("Please enter a valid number");
+                        } else {
+                            break;
+                        }
+                    } else {
+                        System.out.println("Please enter a valid number");
+                        in.next();
+                    }
+                }
+                try{
+                    if (choice == 1){
+                        return load(true, path);
+                    } else {
+                        return load(false, path);
+                    }
+                } catch (IOException | ClassNotFoundException e) {
+                    System.out.println("Unable to load file\n");
+                }
+            }
         }
     }
-
-    public static List<Student> load(boolean binary, String path) throws IOException, ClassNotFoundException{
-        // TODO: Add error checking
-        ObjectInputStream in = new ObjectInputStream(new FileInputStream(path));
-        List<Student> s = (List<Student>) in.readObject();
-        in.close();
-        return s;
-    }
-}
